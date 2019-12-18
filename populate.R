e18e8b1df3886d683e1c789f9ac99d2a2d1181a6
Ana Maria Antoaneta Tiru <Ana.Tiru@insse.ro>
2019-12-18 09:11:34 +0200

    populate with excel

--- populate.R
+++ populate.R
@@ -20,7 +20,9 @@ dbGetQuery(ipcdb, "SELECT * FROM produse")
 # 2. Populate the table "produse"
 # First insert
 df <- cbind(idProduse = c(1), `20170803carrefour.jsprodusv1.csv`[1,])
+df <- data.frame(idProduse = c(1), Denumire = c("test"), Descriere = c("test"), Pret = c(0), Um = c("test"), Data = c("2019-12-12"), idMag = c(1), idSort = c(NA))
 dbSendQuery(ipcdb, 'INSERT INTO produse (idProduse, Denumire, Descriere, Pret, Um, Data, idMag, idSort) VALUES (:idProduse, :Denumire, :Descriere, :Pret, :Um, :Data, :idMag, :idSort);', df)
+
 
 files <- list.files()
 csv <- grep(pattern = ".csv", x = files)
@@ -29,10 +31,31 @@ prod <- grep(pattern = "produsv1", x = files)
 cod <- grep(pattern = "codificat", x = files)
 files <- files[c(prod, cod)]
 
+
+
+
 fl <- files[prod] 
-for (i in fl) {
-  dbSendQuery(ipcdb, 'INSERT INTO produse (Denumire, Descriere, Pret, Um, Data, idMag, idSort) VALUES (:Denumire, :Descriere, :Pret, :Um, :Data, :idMag, :idSort);', i)
+for (i in xls) {
+  print(i)
+  ipcdb <- dbConnect(RSQLite::SQLite(), "IPC.db")
+  try(dbSendQuery(ipcdb, 'INSERT INTO produse (Denumire, Descriere, Pret, Um, Data, idMag, idSort) VALUES (:Denumire, :Descriere, :Pret, :Um, :Data, :idMag, :idSort);', get(i)))
 }
 
+
+
+
+dbGetQuery(ipcdb, "SELECT count(*) FROM produse")
+dbGetQuery(ipcdb, "SELECT * FROM produse")
 dbSendQuery(ipcdb, 'INSERT INTO produse (Denumire, Descriere, Pret, Um, Data, idMag, idSort) VALUES (:Denumire, :Descriere, :Pret, :Um, :Data, :idMag, :idSort);', `20170906m_image.jsprodusv1.csv`)
 dbGetQuery(ipcdb, "delete from produse")
+
+dbGetQuery(ipcdb, "SELECT data, count(idProduse) FROM produse group by data")
+dbGetQuery(ipcdb, "SELECT idMag, count(idProduse) FROM produse group by idMag")
+dbGetQuery(ipcdb, "SELECT strftime('%d',data) from produse limit 10")
+
+x = dbGetQuery(ipcdb, "SELECT * FROM produse where data == \"2017-09-16\"")
+cx <- left_join(`20180713carrefour.jsprodusv1.csv`, x, by = c("Denumire" = "Denumire"))
+
+sel <- dbGetQuery(ipcdb, "SELECT * FROM produse where denumire == \"Rata tanara eviscerata fara organe Kacsa per kg\"")
+dbGetQuery(ipcdb, "SELECT * FROM produse where idProduse == 293348")
+dbGetQuery(ipcdb, "DELETE * FROM produse where DATA == \"2017-09-20\"")
